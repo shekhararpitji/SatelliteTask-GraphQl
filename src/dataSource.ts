3c0e631a30d36d8aa7d73d6925957dfd3827599d
Arpit Shekhar <shekhararpitji@gmail.com>
2023-12-22 19:26:57 +0530

    implemented typeOrm and env

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -4,13 +4,14 @@ import { User } from './entity/User';
 import { Subscription } from './entity/Subscription';
 import { Package } from './entity/Package';
 import { Channel } from './entity/Channel';
-
+import dotenv from 'dotenv'
+dotenv.config();
 export const appDataSource= new DataSource({
 type:"mysql",
 host:"localhost",
-username:"root",
-password:"root",
-database:"dthdb",
+username:process.env.username,
+password:process.env.password,
+database:process.env.database,
 synchronize:true,
 entities:[User,Subscription,Package,Channel],
 
